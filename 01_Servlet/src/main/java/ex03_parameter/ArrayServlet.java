75e493a0fb3fbeb1548ed80178ff20949031e48f
bboyoung1022 <bboyoung1022@naver.com>
2023-05-09 11:11:01 +0900

    0509

---
+++ 01_Servlet/src/main/java/ex03_parameter/ArrayServlet.java
@@ -0,0 +1,54 @@
+package ex03_parameter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.security.KeyStoreUtil;
+
+/**
+ * Servlet implementation class ArrayServlet
+ */
+@WebServlet("/ArrayServlet")
+public class ArrayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ArrayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//요청 인코딩
+		request.setCharacterEncoding("utf-8");
+		
+		response.setContentType("text/html; charset=utf-8");
+		
+		//요청 파라미터(배열)
+		String[] tel = request.getParameterValues("tel"); //보낼때 넘기는 파라미터값
+		String[] hobbies = request.getParameterValues("hobbies");
+		
+		response.getWriter().append("전화번호 : ").append(tel[0]+"-"+tel[1]+"-"+tel[2]).append(", 취미 : "+Arrays.toString(hobbies));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
