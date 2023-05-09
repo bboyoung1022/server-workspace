a5de4fc306b75163699900b6b86edb182d447823
bboyoung1022 <bboyoung1022@naver.com>
2023-05-09 15:07:04 +0900

    0509-1507

--- 01_Servlet/src/main/java/ex06_forward/ForwardServlet2.java
+++ 01_Servlet/src/main/java/ex06_forward/ForwardServlet2.java
@@ -1,4 +1,4 @@
-package ex05_redirect;
+package ex06_forward;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class RedirectServlet
+ * Servlet implementation class ForwardServlet2
  */
-@WebServlet("/RedirectServlet")
-public class RedirectServlet extends HttpServlet {
+@WebServlet("/ForwardServlet2")
+public class ForwardServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public RedirectServlet() {
+    public ForwardServlet2() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,14 +26,11 @@ public class RedirectServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//Redirect : 들어온 요청을 다른 Servlet이나 페이지로 가도록 클라이언트에게 URL을 알려주는 것.
-		//리다이렉트 이전(첫번째 요청)의 파라미터  확인
+
 		String model = request.getParameter("model");
-		System.out.println("RedirectServlet : " + model);
 		
-		//redirect를 이용해서 다른 서블릿(다른 서버경로)으로 이동하기
-		response.sendRedirect("/01_Servlet/RedirectServlet2?model=OLEDTV");
+		System.out.println("ForwardServlet2 : " + model);
+		
 	}
 
 	/**
